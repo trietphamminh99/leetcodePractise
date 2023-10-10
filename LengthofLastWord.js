77a08ebf05e67976e2a65bcba23e02ad4b2a336d
trietpham <trietphamminh99@gmail.com>
2023-10-10 19:14:29 -0400

    add new question

---
+++ LengthofLastWord.js
@@ -0,0 +1,49 @@
+// 58. Length of Last Word
+// Easy
+// 4.1K
+// 204
+// Companies
+// Given a string s consisting of words and spaces, return the length of the last word in the string.
+
+// A word is a maximal
+// substring
+//  consisting of non-space characters only.
+
+
+
+// Example 1:
+
+// Input: s = "Hello World"
+// Output: 5
+// Explanation: The last word is "World" with length 5.
+// Example 2:
+
+// Input: s = "   fly me   to   the moon  "
+// Output: 4
+// Explanation: The last word is "moon" with length 4.
+// Example 3:
+
+// Input: s = "luffy is still joyboy"
+// Output: 6
+// Explanation: The last word is "joyboy" with length 6.
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+    let count = 0
+    for (let i = s.length-1; i >= 0 ; i--)
+    {
+        if(s[i] == ' ')
+        {
+            if(count != 0)
+            {
+                return count
+            }
+        }
+        else{
+            count++
+        }
+    }
+    return count
+};
