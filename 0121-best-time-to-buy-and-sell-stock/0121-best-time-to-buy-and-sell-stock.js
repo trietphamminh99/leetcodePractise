098ee36c828ccc3fdbef2a35a96a83f9a7d5cfda
Triet pham <82898391+trietphamminh99@users.noreply.github.com>
2024-04-27 17:17:24 -0400

    Time: 77 ms (43.80%), Space: 57.5 MB (99.80%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -2,28 +2,28 @@
  * @param {number[]} prices
  * @return {number}
  */
-
-//buyValue = lowest value in the array
-//maxProfit =in the loop differece to the buy date
-//returnprofit
-//profit = maxProfit - buyValue
 var maxProfit = function(prices) {
-    let buyValue = prices[0];
-    let returnProfit = 0
+    //sellz(i) = 1
+    //buy = 1
     
-    for(let i = 0; i <prices.length; i++) 
+    //pro = 2 - 1 = 0 
+    //max = 1
+    
+    let buy = 0;
+    let max = 0;
+    for (let i = 0; i < prices.length;i++)
         {
-            if(prices[i] < buyValue)
-            {
-                buyValue = prices[i]
-            }
-            else{
-                if ((returnProfit < prices[i]-buyValue))
-                    {
-                        returnProfit = prices[i]-buyValue
-                    }
-            }
-            
+            let profit = prices[i] - prices[buy];
+            if (profit > max)
+                {
+                    max = profit
+                }
+            if (profit < 0)
+                {
+                    buy++
+                    buy=i
+                }
         }
-    return returnProfit
+    return max
+    
 };
