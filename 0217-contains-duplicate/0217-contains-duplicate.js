0ede2cfeaffee9fa36f1459e0a6d4c19e1048b38
Triet pham <82898391+trietphamminh99@users.noreply.github.com>
2024-04-26 22:38:23 -0400

    Time: 170 ms (5.08%), Space: 59.5 MB (91.71%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -3,21 +3,16 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-   //scan though the array
-    // haver the same number 2  I will it true
-    // create a dummy array to if we go though it and it not in the array will put it in here 
-     //indexOf will return -1 if it not in the array
-    //and return the postion of the value if it in the array
-    let boolean = false
-    let dummy = []
-    for (let i = 0 ; i < nums.length; i++)
+    let out = false;
+    let nums1 = nums.sort()
+    for(let i = 0; i < nums.length; i++)
         {
-           
-            if(nums.indexOf(nums[i],i+1) !== -1)
+            if(nums1[i] == nums1[i+1])
                 {
-                    boolean = true
-                    return boolean
+                    out = true;
+                    break;
                 }
         }
-    return boolean
+    return out
+    
 };
