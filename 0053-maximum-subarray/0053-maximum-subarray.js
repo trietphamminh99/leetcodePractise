b917325001526738c04123076b53e56e1a185374
Triet pham <82898391+trietphamminh99@users.noreply.github.com>
2024-04-30 21:13:20 -0400

    Time: 59 ms (96.30%), Space: 57.2 MB (88.42%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.js
+++ 0053-maximum-subarray/0053-maximum-subarray.js
@@ -3,33 +3,22 @@
  * @return {number}
  */
 var maxSubArray = function(nums) {
-    //have a loop 
-    //max sum =0
-    // current sum =0
-    // in side the loop => add each element to the current sum 
-    //if the curSum < 0 => curSum = 0
-    //else => if  curSum > maxSum => maxSum = curSum
-    
-    let curSum = 0
-    let maxSum = -999999999999
-    
+    let max = -99999;
+    let cur = 0
     if (nums.length == 1)
-        {
-            maxSum = nums[0]
-            return maxSum
+        {return nums[0]}
+    for (let i = 0; i < nums.length; i++)
+    {
+        
+        cur += nums[i]
+     
+        if(cur > max){
+          max = cur
         }
-    for(let i = 0; i < nums.length; i++)
+        if (cur < 0)
         {
-            curSum = nums[i] + curSum
-            if(curSum > maxSum)
-            {
-                maxSum = curSum
-            }
-            if(curSum < 0)
-                {
-                    curSum = 0
-                }
-          
+            cur = 0;
         }
-    return maxSum
+    }
+        return max
 };
