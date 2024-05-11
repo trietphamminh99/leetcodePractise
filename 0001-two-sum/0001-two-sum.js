596a22f34e21b68ada9e30251333b133c554499a
Triet pham <82898391+trietphamminh99@users.noreply.github.com>
2024-05-11 16:02:11 -0400

    Time: 96 ms (42.35%), Space: 49.4 MB (76.56%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -3,20 +3,18 @@
  * @param {number} target
  * @return {number[]}
  */
-// num1 + num2 = target
-// target - num1 = num2
-
 var twoSum = function(nums, target) {
-    for(let i = 0; i < nums.length; i++)
+    let out = []
+    for (let i = 0; i < nums.length; i++)
         {
-            let sum = target - nums[i]
-            if (nums.indexOf(sum,i+1) != -1)
-                {
-                    let out = []
-                    out.push(i)
-                    out.push(nums.indexOf(sum,i+1))
-                    return out
-                    
-                }
+            let find = target - nums[i];
+            let val  = nums.indexOf(find,i+1)
+           if (val != -1 && val != i)
+               {
+                 out.push(i);
+                 out.push(val)
+                   return out
+               }
+               
         }
-}
+};
