ae8b9b234a1bda75bbb4ff4276216ad1850c3315
Triet pham <82898391+trietphamminh99@users.noreply.github.com>
2024-05-11 17:08:21 -0400

    Time: 119 ms (89.04%), Space: 56.3 MB (95.27%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.js
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -3,12 +3,11 @@
  * @return {boolean}
  */
 var isPalindrome = function(x) {
- let x1 =  String(x).split('');
+ let x1 = x.toString().split(''); 
  let L = 0; 
  let R = x1.length-1;
     while (L < R)
         {
-            console.log(x1[R])
             if(x1[L] != x1[R])
                 {
                     return false
