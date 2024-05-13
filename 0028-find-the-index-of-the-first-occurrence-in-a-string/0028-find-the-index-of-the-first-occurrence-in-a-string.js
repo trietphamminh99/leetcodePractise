3f99842126c04f83b25fe7bc1b5cf88a308e56e1
Triet pham <82898391+trietphamminh99@users.noreply.github.com>
2024-05-13 19:16:08 -0400

    Time: 59 ms (21.53%), Space: 48.5 MB (86.66%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+    return haystack.indexOf(needle)
+    
+    
+};
