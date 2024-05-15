fa6b7af1ef5f89b2649eafd0815c3a945f5c01f1
Triet pham <82898391+trietphamminh99@users.noreply.github.com>
2024-05-15 16:50:29 -0400

    Time: 60 ms (95.41%), Space: 57 MB (92.03%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.js
+++ 0053-maximum-subarray/0053-maximum-subarray.js
@@ -3,22 +3,31 @@
  * @return {number}
  */
 var maxSubArray = function(nums) {
-    let max = -99999;
-    let cur = 0
+    let max = -9999999;
+    let cur = 0;
+
     if (nums.length == 1)
-        {return nums[0]}
-    for (let i = 0; i < nums.length; i++)
-    {
-        
-        cur += nums[i]
-     
-        if(cur > max){
-          max = cur
-        }
-        if (cur < 0)
         {
-            cur = 0;
+            return nums[0];
         }
-    }
-        return max
+   for(let i = 0 ; i < nums.length; i++)
+       {
+       
+         cur = cur + nums[i]
+           
+         if(cur > max)
+               {
+                max = cur
+               }
+   
+           if(cur < 0){
+             cur = 0;
+             
+           }
+           
+    
+          
+       }
+    return max
+       
 };
