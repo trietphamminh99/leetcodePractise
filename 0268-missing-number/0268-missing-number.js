c4cfb2fe967b851933b1e4e69e2b6663ff1e22d5
Triet pham <82898391+trietphamminh99@users.noreply.github.com>
2024-05-18 19:52:20 -0400

    Time: 81 ms (21.36%), Space: 51.7 MB (30.91%) - LeetHub

--- 0268-missing-number/0268-missing-number.js
+++ 0268-missing-number/0268-missing-number.js
@@ -3,15 +3,16 @@
  * @return {number}
  */
 var missingNumber = function(nums) {
-    let cnt = 0;
-
-    const arr = nums.sort((a, b) => a - b);
-    for (let i = 0 ; i < nums.length+1 ; i++)
+    nums.sort((a,b) => (a-b))
+    let cnt = 0
+    for (let i = 0 ; i < nums.length+1; i++)
         {
-            if (arr[i] != cnt)
+            
+            if(nums[i] !== cnt)
                 {
                     return cnt
                 }
             cnt++
         }
+    
 };
