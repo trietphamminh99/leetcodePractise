b9432c124bbf38e8723a4dfd79937a3399ad1b3c
Triet pham <82898391+trietphamminh99@users.noreply.github.com>
2024-05-19 00:56:28 -0400

    Time: 52 ms (63.94%), Space: 48.3 MB (94.81%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.js
+++ 0088-merge-sorted-array/0088-merge-sorted-array.js
@@ -6,26 +6,30 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 var merge = function(nums1, m, nums2, n) {
-    let p1 = m-1;
+    let p1 = m -1;
     let p2 = n-1;
     let i = m+n-1
-    while(p1 >=0 && p2 >=0)
+    while(p1 >= 0 && p2 >= 0)
         {
-            
             if(nums2[p2] > nums1[p1])
                 {
-                    nums1[i] = nums2[p2]
+                    nums1[i] = nums2[p2];
                     p2--
                 }
             else{
+         
+                
                 nums1[i] = nums1[p1]
                 p1--
+               
             }
+             i--
+        }
+    while (p2 >= 0)
+        {
+            nums1[i] = nums2[p2]
+            p2--
             i--
         }
-    while(p2 >=0){
-        nums1[i] = nums2[p2]
-        i--;
-        p2--
-    }
+        
 };
