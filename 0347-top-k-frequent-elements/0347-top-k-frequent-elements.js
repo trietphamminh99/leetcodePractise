69bcd2ac769c6c3208d11a2e1c127935b7dd4ce4
Triet pham <82898391+trietphamminh99@users.noreply.github.com>
2024-05-23 17:27:30 -0400

    Time: 108 ms (5.50%), Space: 56.2 MB (23.56%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
@@ -0,0 +1,53 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var topKFrequent = function(nums, k) {
+    let map = new Map();
+    let cnt = 0 ;
+    let cur = nums[0]
+    let out = []
+    nums.sort((a,b) => (a-b))
+    if (nums.length == 1)
+    {
+        out.push(1)
+        return out
+    }
+    for (let i = 0 ; i <= nums.length; i++)
+        {
+            if (nums[i] != cur)
+                {
+                    if(!map.has(cnt))
+                        {
+                            map.set(cnt,[])
+                        }
+                    map.get(cnt).push(cur)
+                    cur = nums[i]
+                    cnt = 0
+                }
+            cnt++;
+        }
+    console.log(map)
+    for(let i = nums.length; i >=0 ; i--)
+        {
+            if(map.has(i))
+                {
+                    if(k > 0){
+                        let temp = Array.from(map.get(i))
+                        console.log(temp)
+                        for (let j = 0; j < temp.length;j++)
+                             {
+                                 console.log(temp[j])
+                                if(k >0)
+                                    {
+                                        out.push(temp[j])
+                                        k--                                
+                                    }
+                                
+                             }
+                    }
+                }
+        }
+    return out
+};
