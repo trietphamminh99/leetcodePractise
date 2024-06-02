2d2b023d6f98019401af8671c68185b171047243
Triet pham <82898391+trietphamminh99@users.noreply.github.com>
2024-06-02 04:56:13 -0400

    Time: 104 ms (8.21%), Space: 55.9 MB (8.93%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[]} piles
+ * @param {number} h
+ * @return {number}
+ */
+var minEatingSpeed = function(piles, h) {
+    let l = 1;
+    let k = Infinity;
+    let r = Math.max(...piles)
+    while (l <= r)
+        {
+            
+            let m = l + Math.floor((r-l)/2)
+            console.log("m: " + m)
+            let curVal = 0;
+            let i = 0
+            while (i < piles.length)
+                {
+                    curVal += Math.ceil(piles[i] / m)
+                    i++
+                }
+               if (curVal <= h) {
+                k = Math.min(k, m);
+                r = m - 1;
+                }
+                else {
+                    l = m + 1;
+                }
+    
+        }
+   return k
+    
+};
