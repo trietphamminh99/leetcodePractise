af46adf3e89d41a0cb92cf7830144869684b7731
Triet pham <82898391+trietphamminh99@users.noreply.github.com>
2024-06-28 18:51:31 -0400

    Time: 73 ms (52.06%), Space: 50.9 MB (83.59%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -4,19 +4,38 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-    let arrS = s.split('').sort();
-   
-    let arrT = t.split('').sort();
-   
     if (s.length != t.length)
         {
-            return false
+            return false;
         }
-    for (let i = 0 ; i < s.length ; i++ )
+    const map1 = new Map();
+    for(let i = 0; i < s.length; i++)
         {
-            if (arrS [i] != arrT[i]){
-                return false
+            if(!map1.has(s[i]))
+                {
+                    map1.set(s[i],1)
+                }
+            else{
+                map1.set(s[i],map1.get(s[i]) + 1)
             }
+        }
+    const map2 = new Map();
+    for(let i = 0; i < t.length; i++)
+        {
+            if(!map2.has(t[i]))
+                {
+                    map2.set(t[i],1)
+                }
+            else{
+                map2.set(t[i],map2.get(t[i]) + 1)
+            }
+        }
+    for(let k of map1.keys())
+        {
+            if(map1.get(k) != map2.get(k))
+                {
+                    return false;
+                }
         }
     return true
 };
