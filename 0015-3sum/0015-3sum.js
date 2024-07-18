7c3897e13cede1c47ae9cb92493c8b086fd70184
Triet pham <82898391+trietphamminh99@users.noreply.github.com>
2024-07-18 09:39:01 -0400

    Time: 145 ms (87.20%), Space: 64.1 MB (91.65%) - LeetHub

--- 0015-3sum/0015-3sum.js
+++ 0015-3sum/0015-3sum.js
@@ -3,20 +3,22 @@
  * @return {number[][]}
  */
 var threeSum = function(nums) {
+    
     nums = nums.sort((a,b) => (a-b))
-    const out = [];
-    for (let i = 0; i < nums.length; i++)
+    let out = [];
+    let i = 0;
+    for (let i =0; i < nums.length -1 ; i++)
         {
             if (nums[i] == nums[i-1]) continue;
-            let l = i+1;
-            let r = nums.length -1
+            let l = i + 1
+            let r = nums.length - 1;
             while (l < r)
                 {
-                    let sum = nums[i] +nums[l] +nums[r]
-                    if (sum == 0)
-                        {
-                            let temp = [nums[i],nums[l],nums[r]]
-                            out.push(temp)
+                        let sum = nums[i] + nums[l] + nums[r]
+
+                        if(sum == 0){
+                            let arr = [ nums[i] , nums[l] , nums[r]]
+                            out.push(arr)
                             l++;
                             r--;
                             while (l < r && nums[l] == nums[l-1])
@@ -28,15 +30,17 @@ var threeSum = function(nums) {
                                     r--
                                 }
                         }
-                    if (sum < 0)
-                        {
-                            l++
-                        }
-                    if (sum > 0)
-                        {
-                            r--
-                        }
+                        else if (sum < 0)
+                            {
+                                l++
+                            }
+                     else if (sum > 0)
+                            {
+                                r--
+                            }
                 }
+
+            
         }
     return out
 };
